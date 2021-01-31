8bdc4c1afc0832e41220be96282b5f194a9d1054
Julian Rosen <julianrosen@gmail.com>
2021-01-30 23:46:10 -0500

    update start script

--- start_tt.sh
+++ start_tt.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sleep 0.1 # Workaround for some strange bug with ttyd :\
-/home/mud/newpi/tintin/src/tt++ -G /home/mud/newpi/tintin/mud_config
+/home/mud/mudpi/tintin/src/tt++ -G /home/mud/mudpi/tintin/mud_config
 echo "Connection closed"
