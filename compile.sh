fdf6de52b0afd4402f1ea6f428e99f2297d57065
Julian Rosen <julianrosen@gmail.com>
2021-02-06 09:37:44 -0500

    Added license, build guide, update compile.sh

--- compile.sh
+++ compile.sh
@@ -18,6 +18,31 @@ cmake ..
 make
 cd ../..
 
+# Create start script
+echo '#!/bin/bash
+
+port=${1:-16377}
+
+ki()
+{
+    echo -e "\nClosing mudpi..."   
+    /usr/bin/tmux kill-session -t botty
+    PGID=$(ps -o pgid= $$ | grep -o [0-9]*)
+    kill -SIGTERM -"$PGID"
+}
+
+trap ki SIGINT
+echo "Starting mudpi on port $port..."
+cd EmberMUD/src
+./startup 20495 &
+cd ../../ttyd/build
+./ttyd -p "$port" -t cursorBlink=true -t titleFixed=Mudpi ../../tintin/start_tt.sh >/dev/null 2>/dev/null&
+cd ../..
+sleep 2
+/usr/bin/tmux new -d -s botty EmberMUD/bot/botty.py &' > start.sh
+chmod +x start.sh
+
+
 # Create systemd service
 my_dir=$(pwd)
 echo "[Unit]
