7dfbb0fc64faaa2133332211ff88f91d541c5965
Julian Rosen <julianrosen@gmail.com>
2022-01-12 14:59:17 -0500

    URL login

--- start.sh
+++ start.sh
@@ -7,6 +7,7 @@ ki()
     echo -e "\nClosing mudpi..."   
     PGID=$(ps -o pgid= $$ | grep -o [0-9]*)
     kill -SIGTERM -"$PGID"
+    killall ember
 }
 
 trap ki SIGINT
@@ -14,7 +15,7 @@ cd EmberMUD/src
 ./startup 20495 &
 echo "Started EmberMUD on port 20495"
 cd ../../ttyd/build
-./ttyd -p "$port" -t cursorBlink=true -t titleFixed=Mudpi ../../tintin/start_tt.sh >/dev/null 2>/dev/null&
+./ttyd -p "$port" -t cursorBlink=true -t titleFixed=Mudpi --url-arg ../../tintin/start_tt.sh >/dev/null 2>/dev/null&
 echo "Started ttyd on port $port"
 cd ../..
 sleep infinity
--- tintin/start_tt.sh
+++ tintin/start_tt.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 sleep 0.1 # Workaround for some strange bug with ttyd :\
-/home/mudpi/mudpi/tintin/src/tt++ -G /home/mudpi/mudpi/tintin/mud_config
+
+if [ "$#" -eq 0 ]
+then
+    /home/mudpi/mudpi/tintin/src/tt++ -G /home/mudpi/mudpi/tintin/mud_config -e "#var username EMPTY" -e "#var password EMPTY"
+elif [ "$#" -eq 1 ]
+then
+    /home/mudpi/mudpi/tintin/src/tt++ -G /home/mudpi/mudpi/tintin/mud_config -e "#var username $1" -e "#var password EMPTY"
+else
+    /home/mudpi/mudpi/tintin/src/tt++ -G /home/mudpi/mudpi/tintin/mud_config -e "#var username $1" -e "#var password $2"
+fi
+
 #echo "Connection closed"
